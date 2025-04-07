3d7bc57f98571b085f2696c885912a78171e719c
unknown <dapadillar@unal.edu.co>
2025-04-06 22:05:25 -0400

    initial version

---
+++ app/src/test/java/org/example/ddb/ddb.java
@@ -0,0 +1,15 @@
+package org.example.ddb;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/ddb", glue = {"org.example.ddb"}, plugin = "pretty")
+public class ddb {
+
+
+}
